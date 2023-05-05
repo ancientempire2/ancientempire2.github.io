8afde43e0caba2b03aa1ac6b5acd30369a5cdf33
ancientempire2 <132334969+ancientempire2@users.noreply.github.com>
2023-05-05 09:53:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,63 @@
-const cacheName="my-pwa-cache-v.0001";async function updateCache(t){const{request:a}=t,e=await caches.open(cacheName),n=await fetch(a);n.url,await e.put(a,n)}async function fetchRespondWith(t){return fetch(t.request).catch((async a=>{a.message;const e=await caches.open(cacheName),n=await e.match(t.request);if(!n||"404"===String(n.status))throw new Error("no-match");return n}))}const urlStartWithList=["/static/","/manifest.json"];function isUrlStartWithListContainPathname(t){return urlStartWithList.some((a=>t.startsWith(a)))}async function fetchCallBack(t){const{request:a}=t,{method:e,url:n}=a;if("get"!==e.toLowerCase())return;const c=new URL(n),{pathname:s}=c;(isUrlStartWithListContainPathname(s)||"/"===s)&&(t.waitUntil(updateCache(t)),t.respondWith(fetchRespondWith(t)))}self.addEventListener("fetch",fetchCallBack);
+/* global self, caches, fetch, URL */
+
+/* ::
+type ServiceWorkerEvent = {
+    ...Event,
+    waitUntil: (waitFor: Promise<mixed>) => mixed,
+    respondWith: (respondFor: Promise<mixed>) => mixed,
+    request: Request,
+};
+*/
+
+const cacheName = 'my-pwa-cache-v.0001';
+
+async function updateCache(evt /* :: : ServiceWorkerEvent */) {
+    const {request} = evt;
+    const cache = await caches.open(cacheName);
+    const response = await fetch(request);
+
+    console.log('[PWA]: add page to offline:', response.url);
+
+    await cache.put(request, response);
+}
+
+async function fetchRespondWith(evt /* :: : ServiceWorkerEvent */) {
+    return fetch(evt.request).catch(async (error /* :: : Error */) /* :: : Promise<mixed> */ => {
+        console.log('[PWA]: network request Failed. Get content from cache:', error.message);
+        const cache = await caches.open(cacheName);
+        const matching = await cache.match(evt.request);
+
+        if (!matching || String(matching.status) === '404') {
+            console.log('---> matching is wrong', matching);
+            throw new Error('no-match');
+        }
+
+        return matching;
+    });
+}
+
+const urlStartWithList = ['/static/', '/manifest.json'];
+
+function isUrlStartWithListContainPathname(pathname /* :: : string */) /* :: : boolean */ {
+    return urlStartWithList.some((url /* :: : string */) /* :: : boolean */ => pathname.startsWith(url));
+}
+
+// eslint-disable-next-line complexity
+async function fetchCallBack(evt /* :: : ServiceWorkerEvent */) {
+    const {request} = evt;
+    const {method, url} = request;
+
+    if (method.toLowerCase() !== 'get') {
+        return;
+    }
+
+    const urlData = new URL(url);
+    const {pathname} = urlData;
+
+    if (isUrlStartWithListContainPathname(pathname) || pathname === '/') {
+        evt.waitUntil(updateCache(evt));
+        evt.respondWith(fetchRespondWith(evt));
+    }
+}
+
+self.addEventListener('fetch', fetchCallBack);
